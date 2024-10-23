3615c64e84d788cbe65c3632463096a56aec4e42
yanditv <yankur44@gmail.com>
2024-10-23 13:48:42 -0500

    updated cors

--- proyecto-patrones-api/back-end/src/config/cors.js
+++ proyecto-patrones-api/back-end/src/config/cors.js
@@ -1,5 +1,5 @@
 module.exports = {
-    origin: ['http://localhost:3001', 'https://studio.apollographql.com'],
+    origin: ['http://localhost:3000', 'https://studio.apollographql.com'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
